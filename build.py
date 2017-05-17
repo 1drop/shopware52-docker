470c01d85d6a6c49e23c39036a535540c45da02b
Hans Höchtl <hhoechtl@1drop.de>
2017-05-17 18:04:41 +0200

    [TASK] multithread build

--- build.py
+++ build.py
@@ -1,23 +1,31 @@
-import urllib, json, os, re
+import urllib, json, os, re, multiprocessing
 
-response = urllib.urlopen("https://api.github.com/repos/shopware/shopware/tags")
-data = json.loads(response.read())
+def buildWorker(dockerfileTmpl, shopwareVersion, isDev):
+    dockerfile = 'Dockerfile-' + shopwareVersion + ('-dev' if isDev else '')
+    image = '1drop/shopware:' + shopwareVersion + ('-dev' if isDev else '')
+    f = open(dockerfile, 'w')
+    f.write(dockerfileTmpl.replace('{{SHOPWARE_VERSION}}', shopwareVersion))
+    f.close()
+    print 'BULDING IMAGE: ' + image
+    os.system('docker build -f ' + dockerfile + ' -t ' + image + ' .')
+    os.system('docker push ' + image)
+    return
 
-isDev = os.environ.get('IS_DEV', False)
-
-if isDev:
-    dockerfile = open('Dockerfile.dev.tpl', 'r').read()
-else:
-    dockerfile = open('Dockerfile.prod.tpl', 'r').read()
-
-for version in data:
-    m = re.search('^v(\d+\.\d+\.\d+)$', version['name'])
-    if m:
-        shopwareVersion = m.group(1)
-        f = open('Dockerfile', 'w')
-        f.write(dockerfile.replace('{{SHOPWARE_VERSION}}', shopwareVersion))
-        f.close
-        image = '1drop/shopware:' + shopwareVersion + ('-dev' if isDev else '')
-        print 'BULDING IMAGE: ' + image
-        os.system('docker build -t ' + image + ' .')
-        os.system('docker push ' + image)
+if __name__ == '__main__':
+    response = urllib.urlopen("https://api.github.com/repos/shopware/shopware/tags")
+    data = json.loads(response.read())
+    isDev = os.environ.get('IS_DEV', False)
+    if isDev:
+        dockerfileTmpl = open('Dockerfile.dev.tpl', 'r').read()
+    else:
+        dockerfileTmpl = open('Dockerfile.prod.tpl', 'r').read()
+    jobs = []
+    # Warm up cache layers
+    buildWorker(dockerfileTmpl, '5.2.0', isDev)
+    for version in data:
+        m = re.search('^v(\d+\.\d+\.\d+)$', version['name'])
+        if m:
+            shopwareVersion = m.group(1)
+            p = multiprocessing.Process(target=buildWorker, args=(dockerfileTmpl,shopwareVersion,isDev,))
+            jobs.append(p)
+            p.start()
